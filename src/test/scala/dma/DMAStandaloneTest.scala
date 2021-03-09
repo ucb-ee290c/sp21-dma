97e4ea7bd33576c7a61aba68d31766867ec0ba2e
TsaiAnson <ansonytsai@gmail.com>
2021-03-08 23:11:08 -0800

    Added test for when readSizeBytes > beatBytes (tested 8 > 4)

--- src/test/scala/dma/DMAStandaloneTest.scala
+++ src/test/scala/dma/DMAStandaloneTest.scala
@@ -13,21 +13,14 @@ import scala.util.Random
 import verif._
 
 class DMAStandaloneTest extends AnyFlatSpec with ChiselScalatestTester {
-//  it should "elaborate the DMA <> TLXbar/TLRAM Standalone Block" in {
-//    implicit val p: Parameters = VerifTestUtils.getVerifParameters(xLen = 32) // Testing for our 32b RISC-V chip
-//    val dut = LazyModule(new DMATLRAMStandaloneBlock(VerifTestUtils.getVerifTLMasterPortParameters(), VerifTestUtils.getVerifTLSlavePortParameters(),
-//      AddressSet(0x0, 0xffff), 8))
-//    test(dut.module) { _ =>
-//      assert(true)
-//    }
-//  }
 
-  it should "sanity check basic write + read operations (max-read == beatBytes)" in {
+  it should "sanity check basic write + read operations (max-read <= beatBytes)" in {
     val beatBytes = 4
     val pAdderBits = 32
+    val maxReadBytes = 4
     implicit val p: Parameters = VerifTestUtils.getVerifParameters(xLen = 32, beatBytes = beatBytes, pAddrBits = pAdderBits)
     val dut = LazyModule(new DMAStandaloneBlock(VerifTestUtils.getVerifTLMasterPortParameters(),
-      VerifTestUtils.getVerifTLSlavePortParameters(beatBytes = beatBytes, pAddrBits = pAdderBits),4))
+      VerifTestUtils.getVerifTLSlavePortParameters(beatBytes = beatBytes, pAddrBits = pAdderBits),maxReadBytes))
     test(dut.module).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
       // Ensuring that parameters is properly propagated
       assert(beatBytes == dut.dma.writer.module.req.beatBytes)
@@ -37,7 +30,7 @@ class DMAStandaloneTest extends AnyFlatSpec with ChiselScalatestTester {
       val writeDriver = new DecoupledDriverMaster[EE290CDMAWriterReq](c.clock, c.dma_in.write.req)
       val writeTxProto = new DecoupledTX(new EE290CDMAWriterReq(pAdderBits, beatBytes))
       val readDriver = new DecoupledDriverMaster[EE290CDMAReaderReq](c.clock, c.dma_in.read.req)
-      val readTxProto = new DecoupledTX(new EE290CDMAReaderReq(pAdderBits, beatBytes))
+      val readTxProto = new DecoupledTX(new EE290CDMAReaderReq(pAdderBits, maxReadBytes))
 
       // VIP to read responses + data
       val readRespDriver = new DecoupledDriverSlave[EE290CDMAReaderResp](c.clock, c.dma_in.read.resp, 0)
@@ -54,20 +47,19 @@ class DMAStandaloneTest extends AnyFlatSpec with ChiselScalatestTester {
 
       c.clock.step(r.nextInt(10) + 10)
 
-      // TODO Currently fails due to strange bug. See DMA.scala:188
       for (i <- 0 until 20) {
         val writeSizeBytes = r.nextInt(beatBytes) + 1
         val req = GetRandomDMAWriterReq(pAdderBits, writeSizeBytes, beatBytes, r)
         writeDriver.push(writeTxProto.tx(req))
         c.clock.step(3) // Unsure why this is 3
         assert(c.dma_in.writeBusy.peek().litToBoolean)
-        c.clock.step(50)
+        c.clock.step(20)
         assert(!c.dma_in.writeBusy.peek().litToBoolean)
 
-        readDriver.push(readTxProto.tx(EE290CDMAReaderReqHelper(req.addr.litValue(), writeSizeBytes, pAdderBits, beatBytes)))
+        readDriver.push(readTxProto.tx(EE290CDMAReaderReqHelper(req.addr.litValue(), writeSizeBytes, pAdderBits, maxReadBytes)))
         c.clock.step(2)
         assert(c.dma_in.readBusy.peek().litToBoolean)
-        c.clock.step(50)
+        c.clock.step(20)
         assert(!c.dma_in.readBusy.peek().litToBoolean)
 
 //        // Debug printing
@@ -89,19 +81,80 @@ class DMAStandaloneTest extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
 
-//  it should "sanity check max write + read operations" in {
-//    val dut = LazyModule(new DMATLRAMStandaloneBlock(VerifTestUtils.getVerifTLMasterPortParameters(), VerifTestUtils.getVerifTLSlavePortParameters(),
-//      AddressSet(0x0, 0xfffff), 256))
-//    test(dut.module) { c =>
-//
-//    }
-//  }
+  it should "sanity check basic write + read operations (max-read > beatBytes)" in {
+    val beatBytes = 4
+    val pAdderBits = 32
+    val maxReadBytes = 8
+    implicit val p: Parameters = VerifTestUtils.getVerifParameters(xLen = 32, beatBytes = beatBytes, pAddrBits = pAdderBits)
+    val dut = LazyModule(new DMAStandaloneBlock(VerifTestUtils.getVerifTLMasterPortParameters(),
+      VerifTestUtils.getVerifTLSlavePortParameters(beatBytes = beatBytes, pAddrBits = pAdderBits),maxReadBytes))
+    test(dut.module).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
+      // Ensuring that parameters is properly propagated
+      assert(beatBytes == dut.dma.writer.module.req.beatBytes)
+      assert(pAdderBits == dut.dma.paddrBits)
 
-//  it should "sanity check min (0 bytes) write + read operations" in {
-//    val dut = LazyModule(new DMATLRAMStandaloneBlock(VerifTestUtils.getVerifTLMasterPortParameters(), VerifTestUtils.getVerifTLSlavePortParameters(),
-//      AddressSet(0x0, 0xfffff), 256))
-//    test(dut.module) { c =>
-//
-//    }
-//  }
+      // VIP to send requests
+      val writeDriver = new DecoupledDriverMaster[EE290CDMAWriterReq](c.clock, c.dma_in.write.req)
+      val writeTxProto = new DecoupledTX(new EE290CDMAWriterReq(pAdderBits, beatBytes))
+      val readDriver = new DecoupledDriverMaster[EE290CDMAReaderReq](c.clock, c.dma_in.read.req)
+      val readTxProto = new DecoupledTX(new EE290CDMAReaderReq(pAdderBits, maxReadBytes))
+
+      // VIP to read responses + data
+      val readRespDriver = new DecoupledDriverSlave[EE290CDMAReaderResp](c.clock, c.dma_in.read.resp, 0)
+      val respMonitor = new DecoupledMonitor[EE290CDMAReaderResp](c.clock, c.dma_in.read.resp)
+      val readDataDriver = new DecoupledDriverSlave[UInt](c.clock, c.dma_in.read.queue, 0)
+      val dataMonitor = new DecoupledMonitor[UInt](c.clock, c.dma_in.read.queue)
+
+      // VIP to act as backing memory
+      val slaveFn = new TLMemoryModel(dut.to_mem.params)
+      val slaveModel = new TLDriverSlave(c.clock, dut.to_mem, slaveFn, TLMemoryModel.State.empty())
+      val slaveMonitor = new TLMonitor(c.clock, dut.to_mem)
+
+      val r = new Random
+
+      c.clock.step(r.nextInt(10) + 10)
+
+      for (i <- 0 until 20) {
+        val writeSizeBytes = r.nextInt(beatBytes) + 1
+        val req = GetRandomDMAWriterReq(pAdderBits, writeSizeBytes, beatBytes, r)
+        writeDriver.push(writeTxProto.tx(req))
+        c.clock.step(3) // Unsure why this is 3
+        assert(c.dma_in.writeBusy.peek().litToBoolean)
+        c.clock.step(20)
+        assert(!c.dma_in.writeBusy.peek().litToBoolean)
+
+        val readSizeBytes = r.nextInt(maxReadBytes) + 1
+        readDriver.push(readTxProto.tx(EE290CDMAReaderReqHelper(req.addr.litValue(), readSizeBytes, pAdderBits, maxReadBytes)))
+        c.clock.step(2)
+        assert(c.dma_in.readBusy.peek().litToBoolean)
+        c.clock.step(20)
+        assert(!c.dma_in.readBusy.peek().litToBoolean)
+
+        //        // Debug printing
+        //        println(s"Read Driver queue: ${readDriver.inputTransactions.size}")
+        //        val txns = slaveMonitor.getMonitoredTransactions()
+        //        println(s"Montior txns: ${txns.size}")
+        //        txns.foreach(println(_))
+        //        println(s"Data: ${req.data.litValue()}")
+
+        assert(respMonitor.monitoredTransactions.nonEmpty)
+        assert(dataMonitor.monitoredTransactions.nonEmpty)
+        assert(dataMonitor.monitoredTransactions.head.data.litValue() == req.data.litValue())
+        if (readSizeBytes > beatBytes) {
+          // If readSizeBytes > beatBytes, need to check second beat of data
+          assert(dataMonitor.monitoredTransactions.size == 2)
+          // Extracting second beat of data directly from the memory state
+          val bytesPerWord = slaveModel.params.dataBits/8
+          val addr = ((req.addr.litValue() / bytesPerWord) + 1).toLong
+          assert(dataMonitor.monitoredTransactions(1).data.litValue() == TLMemoryModel.read(slaveModel.state.mem, addr, -1, bytesPerWord))
+        }
+        respMonitor.clearMonitoredTransactions()
+        dataMonitor.clearMonitoredTransactions()
+
+        //        // Debug printing
+        //        println(s"Passed $i, ${req.data.litValue()}")
+      }
+    }
+  }
+
 }
--- src/test/scala/dma/DMAUtils.scala
+++ src/test/scala/dma/DMAUtils.scala
@@ -10,8 +10,8 @@ object DMAUtils {
   def EE290CDMAWriterReqHelper(addr: BigInt, data: BigInt, totalBytes: Int, addrBits: Int, beatBytes: Int): EE290CDMAWriterReq = {
     new EE290CDMAWriterReq(addrBits, beatBytes).Lit(_.addr -> addr.U, _.data -> data.U, _.totalBytes -> totalBytes.U)
   }
-  def EE290CDMAReaderReqHelper(addr: BigInt, totalBytes: Int, addrBits: Int, beatBytes: Int): EE290CDMAReaderReq = {
-    new EE290CDMAReaderReq(addrBits, beatBytes).Lit(_.addr -> addr.U, _.totalBytes -> totalBytes.U)
+  def EE290CDMAReaderReqHelper(addr: BigInt, totalBytes: Int, addrBits: Int, maxReadSize: Int): EE290CDMAReaderReq = {
+    new EE290CDMAReaderReq(addrBits, maxReadSize).Lit(_.addr -> addr.U, _.totalBytes -> totalBytes.U)
   }
   def GetRandomDMAWriterReq(addrBits: Int, writeSizeBytes: Int, beatBytes: Int, r: Random): EE290CDMAWriterReq = {
     val addr = BigInt(addrBits, r) & ~BigInt("1" * writeSizeBytes * 8, 2)
